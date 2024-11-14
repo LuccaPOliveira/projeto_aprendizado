4a1ccb798545f6b921a30e7f7fe152f329b8c4d4
Lucca Oliveira <lucca.piasseta@gmail.com>
2024-11-14 15:05:44 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Variáveis
+idade = 28
+nome = 'Lucca'
+sobrenome = 'Piasseta de Oliveira'
+
+print('Aprendendo Python e Git')
+print('Me chamo',nome,sobrenome)
+print('Eu tenho {} anos'.format(idade))
+
+#Operações
+idadeNova = idade + 1
+
+print('Em janeiro farei {} anos'.format(idadeNova))
+
+#Listas
+lista = [1,2,3,4,5]
+
+print(lista)
