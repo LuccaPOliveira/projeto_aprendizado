b0335a7bb04e1ac16eeece9335a3237df54ffe33
Lucca Oliveira <lucca.piasseta@gmail.com>
2024-11-19 12:39:34 -0300

    Estudo de condicionais

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ print('Em janeiro farei {} anos'.format(idadeNova))
 lista = [1,2,3,4,5]
 
 print(lista)
+
+vontade = input('O que você quer comer?')
+
+if vontade == 'Lanche':
+    print('Vá ao Méqui')
+    print('Coma um lanche')
+elif vontade == 'Pizza':
+    print('Vá ao Hut')
+    print('Coma uma pizza')
+elif vontade == 'Mexicano':
+    print('Vá ao Gua.co')
+    print('Coma um burrito')
+else:
+    print('Coma um miojo em casa mesmo!')
+print('Fome saciada!')
